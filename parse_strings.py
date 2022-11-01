8d9c068e419ff4ee81d687f73c0446e2a0aa114d
dispareo <security@dispareo.ch>
2022-11-01 14:38:10 -0700

    Removing unnecessary line

--- parse_strings.py
+++ parse_strings.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env
 import sys
 
-file = sys.arg[1]
-
 with open(sys.argv[1], 'r') as my_file:
     for line in my_file
         if line.startswith("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"):
